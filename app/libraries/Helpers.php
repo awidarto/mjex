9cb00ec9b21103d47c2c6a3e9d27a0e335a0f03b
Andy Awidarto <andy.awidarto@gmail.com>
2014-03-18 09:33:36 +0700

    refresh

--- app/libraries/Helpers.php
+++ app/libraries/Helpers.php
@@ -4,7 +4,7 @@ class Helpers {
 
         if(file_exists(Config::get('ks.picture_path').$delivery_id.'.jpg')){
             if(file_exists(Config::get('ks.thumb_path').'th_'.$delivery_id.'.jpg')){
-                $thumbnail = URL::to('/').'/public/receiver_thumbs/th_'.$delivery_id.'.jpg';
+                $thumbnail = URL::to('/').'/storage/receiver_thumb/th_'.$delivery_id.'.jpg';
                 $thumbnail = sprintf('<img style="cursor:pointer;" class="thumb" alt="'.$delivery_id.'" src="%s?'.time().'" />',$thumbnail);
             }else{
                 $thumbnail = 'nopic';
