bba18717e065b69941099435b4dee437f2a2a6c9
Andy Awidarto <andy.awidarto@gmail.com>
2014-10-09 15:14:12 +0700

    search

--- app/libraries/Helpers.php
+++ app/libraries/Helpers.php
@@ -1,8 +1,10 @@
 <?php
 class Helpers {
     public static function get_thumbnail($delivery_id){
+        $fullpath = public_path().Config::get('ks.thumb_path').'th_'.$delivery_id.'.jpg';
+        print $fullpath;
 
-        if(file_exists(public_path().Config::get('ks.thumb_path').'th_'.$delivery_id.'.jpg')){
+        if(file_exists($fullpath)){
             $thumbnail = URL::to('/').Config::get('ks.thumb_path').'th_'.$delivery_id.'.jpg';
             //$thumbnail = sprintf('<img style="cursor:pointer;" class="thumb" alt="'.$delivery_id.'" src="%s?'.time().'" />',$thumbnail);
         }else{
