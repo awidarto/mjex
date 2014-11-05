4b9f00008c487ca692234ec601102ef0dbe22623
Andy Awidarto <andy.awidarto@gmail.com>
2014-11-05 10:38:01 +0700

    disclaimer

--- app/views/track.blade.php
+++ app/views/track.blade.php
@@ -7,7 +7,7 @@
     <h3>Order Status</h3>
     <hr />
     <form action="{{ URL::to('track')}}" method="post" accept-charset="utf-8">
-      <label for="phone">Phone / Mobile Number:</label>
+      <label for="phone">Phone / Mobile Number / No Invoice Toko:</label>
       <input type="text" name="phone" id="phone" value="" />
       <input type="submit" name="track" id="" value="Track" class="button" />
     </form>
