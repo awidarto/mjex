22c2a2bc6bfc9d3e045acf4a1ea8ef426d42e6c6
Andy Awidarto <andy.awidarto@gmail.com>
2014-12-09 13:09:20 +0700

    total charge

--- app/views/trackresult.blade.php
+++ app/views/trackresult.blade.php
@@ -26,33 +26,8 @@
       Delivery Type :<br />
       <b>{{ $order['delivery_type'] }}</b><br />
       @if($order['delivery_type'] == 'COD' || $order['delivery_type'] == 'CCOD')
-        Price : IDR {{Helpers::idr( (int) $order['total_price'] ) }}<br />
-        Disc : IDR {{Helpers::idr( (int) $order['total_discount'] )}}<br />
-        Tax : IDR {{ Helpers::idr((int) $order['total_tax'] )}}<br />
-        Sub Total : IDR {{ Helpers::idr( ( (int)$order['total_price'] - (int)$order['total_discount'] ) + (int)$order['total_tax'] )}}
-        <br />
-        @if($order['delivery_bearer'] == 'buyer')
-          Delivery charge : IDR {{ Helpers::idr( (int)$order['delivery_cost'] )}}
-        @else
-          Delivery charge : ditanggung toko
-        @endif
-        <br />
-        @if($order['cod_bearer'] == 'buyer')
-          COD surcharge : IDR {{ Helpers::idr( (int)$order['cod_cost'] ) }}
-        @else
-          COD surcharge : ditanggung toko
-
-        @endif
-        <br />
-        <?php
-          $total = ( (int)$order['total_price'] - (int)$order['total_discount'] ) + (int)$order['total_tax'];
-          $cod = ($order['cod_bearer'] == 'buyer')?(int)$order['cod_cost']:0;
-          $delivery = ($order['delivery_bearer'] == 'buyer')?(int)$order['delivery_cost']:0;
-          $total = $total + $cod + $delivery;
-        ?>
-        <br />
         Total Charge :<br />
-        <b>IDR {{ Helpers::idr( $total )}}</b>
+        <b>IDR {{ Helpers::idr( $order['chargeable_amount'] )}}</b>
       @endif
     </p>
     <p>
