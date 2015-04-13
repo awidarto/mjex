32c0eb65e474fe96308c14b5cfe55e0393807971
Andy Awidarto <andy.awidarto@gmail.com>
2015-04-13 09:44:17 +0700

    shop detail

--- app/routes.php
+++ app/routes.php
@@ -629,7 +629,7 @@ Route::get('shop/{id?}',function($id = null){
     if(is_null($id)){
         $shop = false;
     }else{
-        $shop = Shop::where('id', intval($id) )->first();
+        $shop = Shop::where('id', intval($id) )->whereOr('id', $id)->first();
     }
 
     $logshop = ($shop && isset($shop->_id))?$shop->_id:$shop;
